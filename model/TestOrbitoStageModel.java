85c0c60e1934720327b7b5ec44dfed8b0bd88746
levard emmanuel <emmanuel.levard@edu.univ-fcomte.fr>
2025-05-25 21:27:44 +0200

    modifications tests model

--- model/TestOrbitoStageModel.java
+++ model/TestOrbitoStageModel.java
@@ -1,20 +1,37 @@
 package model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.anyInt;
 
 public class TestOrbitoStageModel {
     @Test
     public void testExtendMatrix() {
+        //méthode apparement impossible à tester avec Mockito
+        //puisque Mockito.mock(OrbitoStageModel.class) n'a pas de méthode extend_matrix
+        // les autres méthodes retournent void donc impossible à tester aussi
+        OrbitoStageModel ostm=Mockito.mock(OrbitoStageModel.class);
+        ArrayList<Integer> tmp=new ArrayList<>();
+        ArrayList<ArrayList> extended = new ArrayList<>();
+        tmp.add(0);tmp.add(0);tmp.add(0);tmp.add(0);tmp.add(0);
+        extended.add(tmp);
+        tmp=new ArrayList<>();tmp.add(0);tmp.add(0);tmp.add(0);
+        extended.add(tmp);
+        tmp=new ArrayList<>();tmp.add(0);tmp.add(0);tmp.add(0);
+        extended.add(tmp);
+        tmp=new ArrayList<>();tmp.add(0);tmp.add(0);tmp.add(0);
+        extended.add(tmp);
+        ArrayList<ArrayList> non_extended = new ArrayList<>();
+        tmp=new ArrayList<>();tmp.add(0);tmp.add(0);
+        non_extended.add(tmp);
+        tmp=new ArrayList<>();tmp.add(0);tmp.add(0);
+        non_extended.add(tmp);
+
 
     }
 
-    @Test
-    public void testSetupCallBacks() {
-
-    }
-
-    @Test
-    public void testComputePartyResult() {
-
-    }
 }
