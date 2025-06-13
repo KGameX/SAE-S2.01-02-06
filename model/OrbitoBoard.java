dd302c4194302a3e7a1f1e90368d330d7fdfc9c5
levard emmanuel <emmanuel.levard@edu.univ-fcomte.fr>
2025-06-13 09:23:38 +0200

    algo de m pour vérifier si case libre

--- model/OrbitoBoard.java
+++ model/OrbitoBoard.java
@@ -53,33 +53,154 @@ public class OrbitoBoard extends ContainerElement {
         // if the grid is empty, is it the first turn and thus, all cells are valid
         if (isEmpty()) {
             // i are rows
-            for(int i=0;i<this.GetNbrCol();i++) {
+            for (int i = 0; i < this.GetNbrCol(); i++) {
                 // j are cols
                 for (int j = 0; j < this.GetNbrRow(); j++) {
                     // cols is in x direction and rows are in y direction, so create a point in (j,i)
-                    lst.add(new Point(j,i));
+                    lst.add(new Point(j, i));
                 }
             }
             return lst;
         }
-        if (x==this.GetNbrCol()-1){
+        //bas droit
+        if (x==this.GetNbrCol()-1 &&  y==this.GetNbrRow()-1) {
             if (isEmptyAt(x-1,y)){
                 lst.add(new Point(x-1,y));
             }
-        }
-        if (y==this.GetNbrRow()-1){
+            if (isEmptyAt(x-1,y-1)){
+                lst.add(new Point(x-1,y-1));
+            }
             if (isEmptyAt(x,y-1)){
                 lst.add(new Point(x,y-1));
             }
         }
-        if (x==0){
+        //haut droit
+        else if (x==this.GetNbrCol()-1 &&  y==0){
+            if (isEmptyAt(x-1,y)){
+                lst.add(new Point(x-1,y));
+            }
+            if (isEmptyAt(x,y+1)){
+                lst.add(new Point(x,y+1));
+            }
+            if (isEmptyAt(x-1,y+1)){
+                lst.add(new Point(x-1,y+1));
+            }
+        }
+        // bas gauche
+        else if (x==0 && y==this.GetNbrRow()-1){
+            if (isEmptyAt(x,y-1)){
+                lst.add(new Point(x,y-1));
+            }
+            if (isEmptyAt(x+1,y-1)){
+                lst.add(new Point(x+1,y-1));
+            }
             if (isEmptyAt(x+1,y)){
                 lst.add(new Point(x+1,y));
             }
         }
-        if (y==0){
+        else if (x==0 && y==0){
+            if (isEmptyAt(x+1,y)){
+                lst.add(new Point(x+1,y));
+            }
+            if (isEmptyAt(x+1,y+1)){
+                lst.add(new Point(x+1,y-1));
+            }
+            if (isEmptyAt(x,y+1)){
+                lst.add(new Point(x+1,y));
+            }
+        }
+        else if (x==GetNbrRow()-1){
+            if (isEmptyAt(x-1,y)){
+                lst.add(new Point(x-1,y));
+            }
+            if (isEmptyAt(x-1,y-1)){
+                lst.add(new Point(x-1,y-1));
+            }
+            if (isEmptyAt(x-1,y+1)){
+                lst.add(new Point(x-1,y+1));
+            }
             if (isEmptyAt(x,y+1)){
                 lst.add(new Point(x,y+1));
+            }
+            if (isEmptyAt(x,y-1)){
+                lst.add(new Point(x,y-1));
+            }
+        }
+        else if  (y==GetNbrCol()-1){
+            if (isEmptyAt(x+1,y)){
+                lst.add(new Point(x+1,y));
+            }
+            if (isEmptyAt(x-1,y)){
+                lst.add(new Point(x-1,y));
+            }
+            if (isEmptyAt(x,y-1)){
+                lst.add(new Point(x,y-1));
+            }
+            if (isEmptyAt(x+1,y-1)){
+                lst.add(new Point(x+1,y-1));
+            }
+            if (isEmptyAt(x-1,y-1)){
+                lst.add(new Point(x-1,y-1));
+            }
+        }
+        else if (y==0) {
+            if (isEmptyAt(x+1,y)){
+                lst.add(new Point(x+1,y));
+            }
+            if (isEmptyAt(x-1,y)){
+                lst.add(new Point(x-1,y));
+            }
+            if (isEmptyAt(x,y+1)){
+                lst.add(new Point(x,y+1));
+            }
+            if (isEmptyAt(x+1,y+1)){
+                lst.add(new Point(x+1,y+1));
+            }
+            if (isEmptyAt(x-1,y+1)){
+                lst.add(new Point(x-1,y+1));
+            }
+        }
+        else if (x==0){
+            if (isEmptyAt(x+1,y)){
+                lst.add(new Point(x+1,y));
+            }
+            if (isEmptyAt(x+1,y-1)){
+                lst.add(new Point(x+1,y-1));
+            }
+            if (isEmptyAt(x+1,y+1)){
+                lst.add(new Point(x+1,y+1));
+            }
+            if (isEmptyAt(x,y+1)){
+                lst.add(new Point(x,y+1));
+            }
+            if (isEmptyAt(x,y-1)){
+                lst.add(new Point(x,y-1));
+            }
+        }
+        else {
+            if (isEmptyAt(x-1,y-1)){
+                lst.add(new Point(x-1,y-1));
+            }
+            if (isEmptyAt(x+1,y-1)){
+                lst.add(new Point(x+1,y-1));
+            }
+            if (isEmptyAt(x+1,y+1)){
+                lst.add(new Point(x+1,y+1));
+            }
+            if (isEmptyAt(x-1,y+1)){
+                lst.add(new Point(x-1,y+1));
+            }
+            if (isEmptyAt(x+1,y)){
+                lst.add(new Point(x+1,y));
+            }
+            if  (isEmptyAt(x-1,y)){
+                lst.add(new Point(x-1,y));
+            }
+            if (isEmptyAt(x,y+1)){
+                lst.add(new Point(x,y+1));
+            }
+            if (isEmptyAt(x,y-1)){
+                lst.add(new Point(x,y-1));
             }
         }
 
